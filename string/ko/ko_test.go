aa6103b8ecfa7c144dc725817e6672abea6481d4
HyunSuk Lee <blackknight8012@gmail.com>
2016-03-27 23:30:18 +0900

    add test

--- string/ko/ko_test.go
+++ string/ko/ko_test.go
@@ -11,3 +11,31 @@ func ExampleHasConsonantSuffix() {
 	// true
 	// false
 }
+
+func Example_printBytes() {
+	s := "가나다"
+	for i := 0; i < len(s); i++ {
+		fmt.Printf("%x:", s[i])
+	}
+	fmt.Println()
+	// Output:
+	// ea:b0:80:eb:82:98:eb:8b:a4:
+}
+
+func Example_printBytes2() {
+	s := "가나다"
+	fmt.Printf("%x\n", s)
+	fmt.Printf("% x\n", s)
+	// Output:
+	// eab080eb8298eb8ba4
+	// ea b0 80 eb 82 98 eb 8b a4
+}
+
+func Example_modifyBytes() {
+	b := []byte("가나다")
+	b[2]++
+	b[5]++
+	fmt.Println(string(b))
+	// Output:
+	// 각낙다
+}
